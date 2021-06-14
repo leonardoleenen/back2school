c363a1bad225c785fb98b5c7800a158fc1ad3cf1
Leonardo Leenen <leonardoleenen@gmail.com>
2021-06-14 01:03:18 -0300

    Adding header

--- pages/api/reports/absente.ts
+++ pages/api/reports/absente.ts
@@ -2,6 +2,7 @@ import { firebaseManager } from '../../../services/firebase.service'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function (req, res) {
+  /*
   const raw = req.body.form_response
   const absenteCase: AbsenteCase = {
     alumni: JSON.parse(Buffer.from(raw.hidden.alumni, 'base64').toString()),
@@ -19,14 +20,17 @@ export default async function (req, res) {
     ],
     absenteFrom: raw[0].date,
     absenteTo: raw[1].date
-  }
+  } */
 
   await firebaseManager
     .getDB()
     .collection('bucket')
     .doc(req.body.event_id)
     .set({
-      ...absenteCase
+      ...req.body,
+      alumni: JSON.parse(
+        Buffer.from(req.body.form_response.hidden.alumni, 'base64').toString()
+      )
     })
   res.send('done')
 }
--- pages/declare_absente.tsx
+++ pages/declare_absente.tsx
@@ -3,9 +3,11 @@ import { createWidget } from '@typeform/embed'
 import '@typeform/embed/build/css/widget.css'
 import { authService } from '../services/auth.service'
 import { UIAbsenteStore } from '../stores/absente.store'
+import { useRouter } from 'next/router'
 
 export const DeclareAbsence = (): JSX.Element => {
   const container = useRef()
+  const router = useRouter()
   const almuni: Alumni = UIAbsenteStore.useState(s => s.almuniSelected)
 
   useEffect(() => {
@@ -18,10 +20,31 @@ export const DeclareAbsence = (): JSX.Element => {
     })
   }, [])
   return (
-    <div>
-      <div className="h-24 bg-blue-400">Header</div>
+    <div className="">
+      <div className="flex p-4 pt-8 items-center">
+        <div
+          onClick={() => router.push('/home')}
+          className="flex items-center pr-2 "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1}
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+        </div>
+        <div>Volver al menú principal</div>
+      </div>
       <div>
-        <div className="h-screen" ref={container}></div>
+        <div className=" h-screen" ref={container}></div>
       </div>
     </div>
   )
