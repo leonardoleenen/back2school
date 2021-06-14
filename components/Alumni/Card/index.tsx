2c18c41d1183e4eb0878d668155d62fb9c9f9a41
Leonardo Leenen <leonardoleenen@gmail.com>
2021-06-13 23:29:45 -0300

    absente

--- components/Alumni/Card/index.tsx
+++ components/Alumni/Card/index.tsx
@@ -1,5 +1,6 @@
 import { Avatar } from 'antd'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 interface CardProps {
   alumni: Alumni
@@ -8,13 +9,17 @@ interface CardProps {
 export const Card = (props: CardProps): JSX.Element => {
   const almuni: Alumni = props.alumni
 
+  const router = useRouter()
+
   return (
     <div key={props.alumni.id}>
       <div>
         <Avatar src={almuni.photo} />
       </div>
       <div>{almuni.firstName}</div>
-      <div>Informar Ausentismo</div>
+      <div onClick={() => router.push('/declare_absente')}>
+        Informar Ausentismo
+      </div>
     </div>
   )
 }
