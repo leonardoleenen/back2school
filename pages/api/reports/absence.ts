4ade4198d6e60473be26244cc248622c4248af89
Leonardo Leenen <leonardoleenen@gmail.com>
2021-06-13 23:19:08 -0300

    Adding webhook

--- pages/api/reports/absence.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default async function (req, res) {
-  res.send('done')
-}
--- pages/home_mutifamily.tsx
+++ pages/home_mutifamily.tsx
@@ -3,3 +3,5 @@ import React from 'react'
 const HomeMultiPage = (): JSX.Element => {
   return <div>Multi Family TODO</div>
 }
+
+export default HomeMultiPage
